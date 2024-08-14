ca19e91b3b4a6b49b21e6136e60d079777786cf8
NiloyDas19 <niloydas5215@gmail.com>
2024-08-15 00:33:43 +0600

    dasiy ui set up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <div>
           <h1 className="text-5xl text-red-800 font-bold">Doing Basic Set Up</h1>
+          <button className="btn btn-secondary">button check</button>
       </div>
     </>
   )
