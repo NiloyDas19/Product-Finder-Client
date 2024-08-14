d80ac724103b929ed9bbf00355ad0cf4bd198d4a
NiloyDas19 <niloydas5215@gmail.com>
2024-08-15 00:26:14 +0600

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1 className="text-5xl text-red-800 font-bold">Doing Basic Set Up</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
